5e9e8e780561f6c7bbc20b88fae64fa9550db60c
Rajendran Mariya <r.mariya@webandappstudio.com>
2020-09-03 22:53:50 +0530

    change response details in customer api

--- api/application/controllers/ProductController.php
+++ api/application/controllers/ProductController.php
@@ -33,6 +33,7 @@ class ProductController extends API_Controller{
 		$this->load->model('ProductModel');
 		$json_request_body = file_get_contents('php://input');
 		$data = json_decode($json_request_body, true);
+		$resultSet = Array();
 
 		if(isset($data['search_keyword']) && isset($data['page_count']) && isset($data['page_limits'])){
 			$search_keyword = $data['search_keyword'];
@@ -44,6 +45,7 @@ class ProductController extends API_Controller{
 					'code' => HTTP_201,
 					'isSuccess' => false,
 					'message' => NEED_PAGE_COUNT,
+					'customer_details' => $resultSet
 				);
 				$this->output
 				->set_content_type('application/json')
@@ -54,7 +56,7 @@ class ProductController extends API_Controller{
 				$page_count = ($page_count * $page_limits);
 				$result_query = $this->ProductModel->getAllProductDetails($search_keyword,$page_count,$page_limits);
 			//print_r($result_query);
-				$resultSet = Array();
+				
 				if($result_query)
 				{
 					foreach ($result_query as $product_result) 
@@ -73,7 +75,7 @@ class ProductController extends API_Controller{
 						'code' => HTTP_200,
 						'isSuccess' => true,
 						'message' => PRODUCT_RECEIVED,
-						'product_details' => $resultSet
+						'customer_details' => $resultSet
 					);
 					$this->output
 					->set_content_type('application/json')
@@ -85,7 +87,7 @@ class ProductController extends API_Controller{
 						'code' => HTTP_201,
 						'isSuccess' => false,
 						'message' => NEED_SEARCH_RESULT,
-						'product_details' => $resultSet
+						'customer_details' => $resultSet
 					);
 					$this->output
 					->set_content_type('application/json')
@@ -98,7 +100,8 @@ class ProductController extends API_Controller{
 			$response_array = array(
 				'code' => HTTP_201,
 				'isSuccess' => false,
-				'message' => NEED_ALL_PARAMS
+				'message' => NEED_ALL_PARAMS,
+				'customer_details' => $resultSet
 			);
 			$this->output
 			->set_content_type('application/json')
